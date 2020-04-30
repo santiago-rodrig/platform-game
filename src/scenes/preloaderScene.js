17840345c7611f8bf2aefd034be3c817577186b6
Santiago Rodríguez <santo1996.29@gmail.com>
2020-04-30 18:45:59 -0400

    preload function for preloader scene, stage 1

--- src/scenes/preloaderScene.js
+++ src/scenes/preloaderScene.js
@@ -2,4 +2,57 @@ export class PreloaderScene extends Phaser.Scene {
   constructor() {
     super('PreloaderScene');
   }
+
+  preload() {
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
+    const width = this.cameras.main.width;
+    const height = this.cameras.main.height;
+
+    const loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff'
+      }
+    });
+
+    const percentText = this.make.text({
+      x: width / 2,
+      y: height / 2,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff'
+      }
+    });
+
+    const assetText = this.make.text({
+      x: width / 2,
+      y: height / 2 + 100,
+      text: 'Initializing...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff'
+      }
+    });
+
+    assetText.setOrigin(0.5, 0.5);
+    percentText.setOrigin(0.5, 0.5);
+    loadingText.setOrigin(0.5, 0.5);
+    progressBox.fillStyle(0x222222, 0.8);
+
+    progressBox.fillRect(
+      width / 2,
+      height / 2,
+      320,
+      50
+    );
+
+    this.load.on('progress', function (value) {
+      
+    });
+  }
 }
