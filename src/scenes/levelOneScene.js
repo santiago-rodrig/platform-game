4191ccacdcbcc84120a2414e00472747f9b1e1a4
Santiago Rodríguez <santo1996.29@gmail.com>
2020-05-05 12:59:54 -0400

    add sound effects and footsteps for player

--- src/scenes/levelOneScene.js
+++ src/scenes/levelOneScene.js
@@ -11,6 +11,12 @@ export class LevelOneScene extends Phaser.Scene {
     this.add.image(width / 2, height / 2 - 212, 'background');
     this.add.image(width / 2 + 1024, height / 2 - 212, 'background');
 
+    this.footstepAudio = this.sound.add(
+      'footstep',
+      { volume: 0.5, loop: true, rate: 0.75 }
+    );
+
+    this.footstepSAudioIsPlaying = false;
     this.cursors = this.input.keyboard.createCursorKeys();
     this.platforms = this.physics.add.staticGroup();
 
@@ -18,6 +24,8 @@ export class LevelOneScene extends Phaser.Scene {
     // 10 * 64 + 32 = 672
     this.addPlatforms(672, height - 32, 1, 20);
     this.buttonGoal = this.physics.add.sprite(600, height - 96, 'objects', 96);
+    this.buttonGoal.body.setSize(25, 50);
+    this.buttonGoal.body.setOffset(0, 13);
 
     this.anims.create({
       key: 'advance',
@@ -68,24 +76,38 @@ export class LevelOneScene extends Phaser.Scene {
           this.player.setVelocityY(-500);
           this.player.anims.stop();
           this.player.setFrame(1);
+          this.footstepAudio.stop();
+          this.footstepSAudioIsPlaying = false;
         } else if (this.cursors.right.isDown) {
-          this.player.setVelocityX(160);
+          this.player.setVelocityX(200);
           this.player.flipX = false;
 
           if (!this.player.anims.isPlaying) {
             this.player.anims.play('advance');
           }
+
+          if (!this.footstepSAudioIsPlaying) {
+            this.footstepAudio.play();
+            this.footstepSAudioIsPlaying = true;
+          }
         } else if (this.cursors.left.isDown) {
-          this.player.setVelocityX(-160);
+          this.player.setVelocityX(-200);
           this.player.flipX = true;
 
           if (!this.player.anims.isPlaying) {
             this.player.anims.play('advance');
           }
+
+          if (!this.footstepSAudioIsPlaying) {
+            this.footstepAudio.play();
+            this.footstepSAudioIsPlaying = true;
+          }
         } else {
           this.player.setVelocityX(0);
           this.player.anims.stop();
           this.player.setFrame(0);
+          this.footstepAudio.stop();
+          this.footstepSAudioIsPlaying = false;
         }
       }
     }
--- src/scenes/preloaderScene.js
+++ src/scenes/preloaderScene.js
@@ -81,6 +81,10 @@ export class PreloaderScene extends Phaser.Scene {
 
     this.time.delayedCall(2000, this.ready, [], this);
     this.load.audio('backgroundMusic', ['assets/backgroundMusic.mp3']);
+    this.load.audio('footstep', ['assets/footstep.ogg']);
+    this.load.audio('levelCompleted', ['assets/levelCompleted.ogg']);
+    this.load.audio('jewelGathering', ['assets/jewelGathering.ogg']);
+    this.load.audio('gameOver', ['assets/gameOver.ogg']);
 
     this.load.spritesheet('ui', 'assets/userInterface/ui.png', {
       frameWidth: 16,
