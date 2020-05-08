673ed5e1a06f0b3eac9395eb18145689ae55ea0b
Santiago Rodríguez <santo1996.29@gmail.com>
2020-05-08 18:13:22 -0400

    fix path to scores helper

--- src/scoresFetcher.js
+++ src/scoresFetcher.js
@@ -1,4 +1,4 @@
-import ScoresHelper from '../helpers/scoresHelper';
+import ScoresHelper from './helpers/scoresHelper';
 
 const API = {
   scoresUrl: 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/cASPRoHtswEsGw9hjO5y/scores/'
