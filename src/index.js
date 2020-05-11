8a3a2ea6cbb292bfe91201301bcb075183dad125
Santiago Rodríguez <santo1996.29@gmail.com>
2020-05-11 12:09:54 -0400

    add style files to loader script

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import { TitleScene } from './scenes/titleScene';
 import { OptionsScene } from './scenes/optionsScene';
 import { CreditsScene } from './scenes/creditsScene';
 import { GameScene } from './scenes/gameScene';
-import 'materialize-css/dist/js/materialize';
-import 'materialize-css/dist/css/materialize.css';
-import './scss/index.scss';
 import './DOMManager';
 import DOMManager from './DOMManager';
 
